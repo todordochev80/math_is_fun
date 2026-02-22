18f51c2737198f75fc0cc369840e22fae9188264
todordochev80 <t.dochev80@gmail.com>
2026-02-22 19:44:22 +0000

    Initial commit

---
+++ math_is_fun/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for math_is_fun project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'math_is_fun.settings')
+
+application = get_asgi_application()
